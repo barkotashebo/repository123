afe2f156024cff0e8176ef59af77ec775a2d7646
barkotashebo <50146919+barkotashebo@users.noreply.github.com>
2019-04-30 12:43:34 -0400

    Add files via upload
    new file from the doc

---
+++ order.js
@@ -0,0 +1,17 @@
+[{
+"orderiD" : 592,
+"shipping":"internantional",
+"item":"T-shirt Navy",
+"size":"L",
+"quantity":1
+"price":20
+},
+{
+"orderiD" : 972,
+"shipping":"domestic",
+"item":"Cargo Shorts",
+"size":"XL"
+"quantity":2
+"price":20
+}
+]
